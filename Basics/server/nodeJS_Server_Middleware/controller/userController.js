32025b627fb94b27319d08f0b406c8f549448488
huzaifa003 <buntyhuzaifa3@gmail.com>
2022-07-15 21:12:39 +0500

    Added the sign up method

--- Basics/server/nodeJS_Server_Middleware/controller/userController.js
+++ Basics/server/nodeJS_Server_Middleware/controller/userController.js
@@ -1,5 +1,64 @@
-express = require('express')
-router = express.Router()
-path = require('path')
-bcrypt = require('bcrypt')
+const express = require('express')
+const router = express.Router()
+const path = require('path')
+const bcrypt = require('bcrypt')
+const fs = require('fs').promises
 
+const userData = {
+    users: require('../data/user.json')
+}
+
+async function writeFile() {
+    
+}
+
+const addUser = async (req,res) =>{
+    
+    const username = req.body.username
+    const password = req.body.password
+    console.log(userData.users)
+    const duplicate = userData.users.find(person=> person.username === username)
+    if (duplicate)
+    {
+        return res.status(409).send("This user already exists") //409 Represents conflict
+    }    
+
+    
+    try {
+        const hashedValue = await bcrypt.hash(password,10) //10 is salt for encryption standard is 10
+        const newUser = {
+            username : req.body.username,
+            password : hashedValue
+        }
+        userData.users.push(newUser)
+
+        await fs.writeFile(path.join(__dirname,'..','data','user.json'),JSON.stringify(userData.users), (err)=>{
+            if (err)
+            {
+                return err.message
+            }
+        })
+        res.status(201).json({
+            "Success": `${newUser.username} Created`
+        })
+    } catch (error) {
+        res.status(500).json({
+            'message': error.message
+        })
+    }
+}
+
+const checkUser = (req,res)=>{
+    const username = req.body.username
+    const password = req.body.password
+
+    const person = userData.users.find(person=> person.username === username)
+    if (!person){
+        return 
+    }
+}
+
+
+module.exports = {
+    addUser
+}
--- Basics/server/nodeJS_Server_Middleware/router/api/user.js
+++ Basics/server/nodeJS_Server_Middleware/router/api/user.js
@@ -1,17 +1,12 @@
 
 const express = require('express')
 const router = express.Router()
-const path = require('path')
 
 const userController = require('../../controller/userController')
 
-const userData = {
-    users: require('../../data/user.json')
-}
+
 
 router.route('/|/register(.html)')
-    .get((req,res)=>{
-        res.json(userData.users)
-    })
+    .post(userController.addUser)
 
 module.exports = router
