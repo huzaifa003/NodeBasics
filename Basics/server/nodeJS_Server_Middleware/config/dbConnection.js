e9e706b4dcb9389142252d79a767600e41248987
huzaifa003 <buntyhuzaifa3@gmail.com>
2022-07-18 16:53:18 +0500

    Added the DB Environment and Connection String

---
+++ Basics/server/nodeJS_Server_Middleware/config/dbConnection.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const connectDB =  async ()=>{
+    try {
+        await mongoose.connect(process.env.DB_URL,{
+            useUnifiedTopology: true,
+            useNewUrlParser: true
+        })
+    } catch (err) {
+        console.log(err,"Error coming from DB")
+    }
+}
+
+module.exports = connectDB
